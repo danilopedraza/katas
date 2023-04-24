216c9cdbe4f8382d8815ae8cf535c7cd017d09f3
cpedraza <cpedraza@unal.edu.co>
2023-04-23 22:29:55 -0500

    more refactoring

--- 004-gilded-rose/src/gildedrose.rs
+++ 004-gilded-rose/src/gildedrose.rs
@@ -36,7 +36,7 @@ impl GildedRose {
         }
     }
 
-    fn evaluate_item_quality(item: &mut Item) {
+    fn update_item_quality(item: &mut Item) {
         if item.name == "Backstage passes to a TAFKAL80ETC concert" {
             if 0 <= item.sell_in && item.sell_in <= 5 {
                 item.quality += 3;
@@ -45,28 +45,27 @@ impl GildedRose {
             } else {
                 item.quality += 1;
             }
-
-            if item.quality > 50 {
-                item.quality = 50;
-            }
-
         } else if item.name == "Aged Brie" {
-            if item.quality >= 50 {
-                return;
-            }
-
             item.quality = item.quality + 1;
         } else {
-            if item.quality > 0 {
-                if item.name != "Sulfuras, Hand of Ragnaros" {
-                    item.quality = item.quality - 1;
-                }
-            }
+            item.quality = item.quality - 1;
+        }
+
+        if item.quality > 50 {
+            item.quality = 50;
+        }
+        
+        if item.quality < 0 {
+            item.quality = 0;
         }
     }
 
-    fn update_item_quality(item: &mut Item) {
-            Self::evaluate_item_quality(item);
+    fn update_item(item: &mut Item) {
+            if item.name == "Sulfuras, Hand of Ragnaros" {
+                return;
+            }
+
+            Self::update_item_quality(item);
         
 
             Self::update_item_sell_in(item);
@@ -75,12 +74,10 @@ impl GildedRose {
                 if item.name != "Aged Brie" {
                     if item.name != "Backstage passes to a TAFKAL80ETC concert" {
                         if item.quality > 0 {
-                            if item.name != "Sulfuras, Hand of Ragnaros" {
                                 item.quality = item.quality - 1;
-                            }
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if item.quality < 50 {
@@ -92,7 +89,7 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for item in &mut self.items {
-            Self::update_item_quality(item);
+            Self::update_item(item);
         }
     }
 }

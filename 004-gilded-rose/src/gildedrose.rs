e0f68c724cb2e67f03e2c7e570e0918f3f04bf0e
cpedraza <cpedraza@unal.edu.co>
2023-04-23 23:04:00 -0500

    looks like enough BL refactoring

--- 004-gilded-rose/src/gildedrose.rs
+++ 004-gilded-rose/src/gildedrose.rs
@@ -31,24 +31,32 @@ impl GildedRose {
     }
 
     fn update_item_sell_in(item: &mut Item) {
-        if item.name != "Sulfuras, Hand of Ragnaros" {
-            item.sell_in = item.sell_in - 1;
-        }
+        item.sell_in = item.sell_in - 1;
     }
 
     fn update_item_quality(item: &mut Item) {
         if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-            if 0 <= item.sell_in && item.sell_in <= 5 {
+            if 0 <= item.sell_in && item.sell_in <= 4 {
                 item.quality += 3;
-            } else if 6 <= item.sell_in && item.sell_in <= 10 {
+            } else if 5 <= item.sell_in && item.sell_in <= 9 {
                 item.quality += 2;
+            } else if item.sell_in < 0 {
+                item.quality = 0;
             } else {
                 item.quality += 1;
             }
         } else if item.name == "Aged Brie" {
-            item.quality = item.quality + 1;
+            if item.sell_in < 0 {
+                item.quality = item.quality + 2;
+            } else {
+                item.quality = item.quality + 1;
+            }
         } else {
-            item.quality = item.quality - 1;
+            if item.sell_in < 0 {
+                item.quality = item.quality - 2;
+            } else {
+                item.quality = item.quality - 1;
+            }
         }
 
         if item.quality > 50 {
@@ -61,30 +69,13 @@ impl GildedRose {
     }
 
     fn update_item(item: &mut Item) {
-            if item.name == "Sulfuras, Hand of Ragnaros" {
-                return;
-            }
+        if item.name == "Sulfuras, Hand of Ragnaros" {
+            return;
+        }
 
-            Self::update_item_quality(item);
-        
+        Self::update_item_sell_in(item);
 
-            Self::update_item_sell_in(item);
-
-            if item.sell_in < 0 {
-                if item.name != "Aged Brie" {
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if item.quality > 0 {
-                                item.quality = item.quality - 1;
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if item.quality < 50 {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+        Self::update_item_quality(item);
     }
 
     pub fn update_quality(&mut self) {

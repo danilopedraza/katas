bd1e4205ff26d463d31fc0ba64bfad35af0fe3a5
cpedraza <cpedraza@unal.edu.co>
2023-05-20 20:46:06 -0500

    changed some names

--- 007-potter/src/potter.ts
+++ 007-potter/src/potter.ts
@@ -1,10 +1,10 @@
 export class PotterPriceCalculator {
-    multiplierArray: number[];
+    multipliers: number[];
     constructor() {
-        this.multiplierArray = [1.00, 1.00, 0.95, 0.90, 0.80, 0.75];
+        this.multipliers = [1.00, 1.00, 0.95, 0.90, 0.80, 0.75];
     }
 
-    public getPrice(books: number[]) : number {
+    public price(books: number[]) : number {
         return books.length * 8;
     }
 }
--- 007-potter/tests/ocr.spec.ts
+++ 007-potter/tests/ocr.spec.ts
@@ -2,15 +2,15 @@ import { PotterPriceCalculator } from '../src/potter';
 
 describe('The Potter price calculator', () => {
     it('should return 0 when there are no copies', () => {
-        expect(new PotterPriceCalculator().getPrice([])).toEqual(0);
+        expect(new PotterPriceCalculator().price([])).toEqual(0);
     });
 
     it("should return 8 when there is a single copy", () => {
-        expect(new PotterPriceCalculator().getPrice([1])).toEqual(8);
+        expect(new PotterPriceCalculator().price([1])).toEqual(8);
     });
 
     it("should make no discounts when there are only copies of a single book", () => {
-        expect(new PotterPriceCalculator().getPrice([1,1,1])).toEqual(24);
+        expect(new PotterPriceCalculator().price([1,1,1])).toEqual(24);
     });
 
     // it("should make a discount for two groups of four books", () => {

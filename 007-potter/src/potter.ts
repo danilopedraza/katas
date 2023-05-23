550304c1ac9802a96b297b97b23fe6ff50394802
cpedraza <cpedraza@unal.edu.co>
2023-05-23 17:30:31 -0500

    renamed a function

--- 007-potter/src/potter.ts
+++ 007-potter/src/potter.ts
@@ -4,7 +4,7 @@ export class PotterPriceCalculator {
         this.multipliers = [1.00, 1.00, 0.95, 0.90, 0.80, 0.75];
     }
 
-    public price(books: number[]) : number {
+    public getPrice(books: number[]) : number {
         return books.length * 8;
     }
 }
